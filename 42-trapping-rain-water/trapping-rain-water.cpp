c30d7dc9ca19d0a3e34a29029605639c1d4a6487
Priyanshu Shekhar <priyanshudecode@gmail.com>
2026-06-20 15:37:48 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (43.13%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,24 +1,58 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int left = 0;
-        int right = height.size() - 1;
-        int leftMax = height[left];
-        int rightMax = height[right];
-        int water = 0;
+        // int left = 0;
+        // int right = height.size() - 1;
+        // int leftMax = height[left];
+        // int rightMax = height[right];
+        // int water = 0;
 
-        while (left < right) {
-            if (leftMax < rightMax) {
-                left++;
-                leftMax = max(leftMax, height[left]);
-                water += leftMax - height[left];
-            } else {
-                right--;
-                rightMax = max(rightMax, height[right]);
-                water += rightMax - height[right];
-            }
+        // while (left < right) {
+        //     if (leftMax < rightMax) {
+        //         left++;
+        //         leftMax = max(leftMax, height[left]);
+        //         water += leftMax - height[left];
+        //     } else {
+        //         right--;
+        //         rightMax = max(rightMax, height[right]);
+        //         water += rightMax - height[right];
+        //     }
+        // }
+
+        int n = height.size();
+
+        int prev[n];
+        prev[0] = height[0];
+
+        int mx = height[0];
+
+        for(int i=1;i<n;i++){
+            mx = max(mx,height[i]);
+            prev[i] = mx;
         }
 
-        return water;        
-    }
+        int next[n];
+        next[n-1] = height[n-1];
+
+        mx = height[n-1];
+
+        for(int i=n-2;i>=0;i--){
+            mx = max(mx,height[i]);
+            next[i] = mx;
+        }
+
+        int arr[n];
+
+        for(int i=0;i<n;i++){
+            arr[i] = min(prev[i],next[i]);
+        }
+
+        int water = 0;
+
+        for(int i=0;i<n;i++){
+            water += arr[i] - height[i];
+        }
+
+        return water;
+        }
 };
