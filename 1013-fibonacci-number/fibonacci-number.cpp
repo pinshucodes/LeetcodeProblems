8bb2d8553b01fe7da81c18e97c2065d896455d73
Priyanshu Shekhar <priyanshudecode@gmail.com>
2026-08-04 20:05:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (11.14%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -8,11 +8,14 @@ public:
         return v[n] = fibo(n - 1, v) + fibo(n - 2, v);
     }
     int fib(int n) {
+        // Time Complexity- O(n)
+        // space complexity- O(n)
         vector<int> v(n+1, -1);
         return fibo(n, v);
     }
 };
 
         // Time Complexity- O(2^n)
+        // space complexity- O(n)
         // if (n <= 1) return n;
         // return fib(n - 1) + fib(n - 2);
